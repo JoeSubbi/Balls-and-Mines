dd29939c464750e767c29d34114de6830c429fe4
Sophie Boyle <sophieboyle4028@gmail.com>
2022-02-05 01:02:53 +0000

    Tried to get bomb detonation working

---
+++ Assets/Balls-and-Mines/BombDetection.cs
@@ -0,0 +1,47 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BombDetection : MonoBehaviour
+{
+    private SphereCollider collider;
+    public SphereCollider hamster;
+
+    private float entryTime;
+    private float detonationTime;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        collider = GetComponent<SphereCollider>();
+        entryTime = 0.0F;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        Debug.Log("AAAAAAAAAAAAAAAAAAAAAH");
+        CheckIfHamster();
+    }
+
+    void CheckIfHamster()
+    {
+        if (Vector3.Distance(hamster.transform.position, this.transform.position)
+            < (collider.radius + hamster.radius)){
+            if (entryTime != 0.0F){
+                if (Time.time >= detonationTime){
+                    // EXPLODE
+                    Debug.Log("EXPLODE!!!");
+                }
+            } else {
+                // Set entryTime and detonationTime
+                entryTime = Time.time;
+                detonationTime = Time.time + 2;
+                Debug.Log("Detonation time set");
+            }
+        }
+        // Play sound
+        // Set timer
+        // Explode!!!
+    }
+}
